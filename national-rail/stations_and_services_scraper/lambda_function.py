c75c911cd3c86638324ad0d620fa1b4bc3538e85
Weizhi Luo <weizhi.luo@googlemail.com>
2022-02-28 00:59:47 +0000

    Add AWS lambda function and handler.

--- national-rail/stations_and_services_scraper/lambda_function.py
+++ national-rail/stations_and_services_scraper/lambda_function.py
@@ -10,5 +10,5 @@ def lambda_handler(event, context):
     return {
         'statusCode': 200,
         'body': json.dumps('Finished scraping and publishing '
-                           'stations and services')
+                           'stations and services.')
     }
